f92c9a30e68ef215b090b44b6be505a8c68de2ea
MarcusTw <e0014884@u.nus.edu>
2021-09-20 15:12:01 +0800

    add cors

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 const mongoUrl = process.env.MONGODB_URL ?? 'mongodb://localhost/users';
 
