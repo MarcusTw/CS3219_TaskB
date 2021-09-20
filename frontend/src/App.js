13a2a4f83a295e73a60a79e42310f7c78e4e518b
marcustw <e0014884@u.nus.edu.sg>
2021-09-21 00:07:02 +0800

    complete frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import UserForm from './components/UserForm';
 import './App.css';
 import { GET_USERS } from './routes';
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 class App extends Component {
   constructor() {
     super();
@@ -13,11 +17,43 @@ class App extends Component {
       error: null,
       selectedUser: null
     };
+    this.getUsers = this.getUsers.bind(this);
+    this.deleteUserOnCallback = this.deleteUserOnCallback.bind(this);
+    this.editUserOnCallback = this.editUserOnCallback.bind(this);
+    this.randomizeUserOnCallback = this.randomizeUserOnCallback.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    
   }
 
   handleClick = (name) => {
     const selectedUser = this.state.usersData.find(user => user.name === name)
     this.setState(() => ({selectedUser}))
+  }
+
+  deleteUserOnCallback = () => {
+    this.setState({selectedUser: null});
+  }
+
+  editUserOnCallback = () => {
+    GET_USERS().then((res) => {
+      this.setState({usersData: res.data.data});
+      const name = this.state.selectedUser.name;
+      const selectedUser = this.state.usersData.find(user => user.name === name)
+      this.setState(() => ({selectedUser}));
+    }); 
+  }
+
+  randomizeUserOnCallback = () => {
+    const random = getRandomInt(this.state.usersData.length);
+    const user = this.state.usersData[random];
+    this.setState({selectedUser: user});
+  }
+
+  getUsers = () => {
+    GET_USERS().then((res) => {
+      this.setState({usersData: res.data.data})
+    })
+    .catch((err) => console.log(err));
   }
 
   render() {
@@ -30,18 +66,18 @@ class App extends Component {
           <h2>Random Asians</h2>
           <ul>{usersList}</ul>
         </div>
-        <UserDetails user={this.state.selectedUser} />
-        <UserForm />
+        <UserDetails user={this.state.selectedUser} deleteCallback={this.deleteUserOnCallback} editCallback={this.editUserOnCallback} randomizeCallback={this.randomizeUserOnCallback}/>
+        <UserForm isCreate={true}/>
       </div>
     );
   }
 
   componentDidMount() {
-    GET_USERS()
-      .then(res => {
-        this.setState({usersData: res.data.data})
-      })
-      .catch(err => console.log(err));
+    this.getUsers();
+  }
+
+  componentDidUpdate() {
+    this.getUsers();
   }
 }
 
