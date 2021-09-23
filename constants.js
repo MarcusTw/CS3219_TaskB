83f37e8b0f8a1de61732696ae67ec36a7d9c0cfa
MarcusTw <e0014884@u.nus.edu>
2021-09-23 16:48:49 +0800

    change prod link

--- constants.js
+++ constants.js
@@ -1,7 +1,11 @@
 const success = "success";
 
-const apiMsg = "This is API Message test CD";
+const apiMsg = "Welcome to CS3219 Task B done by Marcus. To use my api, use /api.";
 
-const welcomeMsg = "Welcome! Let us test CD!";
+const welcomeMsg = "Welcome to Random Asian User Api. Use /users to see all the users in the database :).";
 
 module.exports = {success, apiMsg, welcomeMsg};
+
+const gcloud_api = "https://cs3219-taskb-m4jbzqtwiq-de.a.run.app/api/users";
+
+const heroku_api = "https://random-asian-user-api.herokuapp.com/";
