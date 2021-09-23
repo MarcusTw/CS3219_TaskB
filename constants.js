ee8330401d236e32ba1a2b271c6d121d74e8b5d8
MarcusTw <e0014884@u.nus.edu>
2021-09-23 15:22:59 +0800

    change welcome msg

--- constants.js
+++ constants.js
@@ -2,6 +2,6 @@ const success = "success";
 
 const apiMsg = "Welcome to CS3219 Task B done by Marcus Tan. Use /api to use the Random Asian User API:)."
 
-const welcomeMsg = "Test Cloud Run CD";
+const welcomeMsg = "Welcome! Let us test CD!";
 
 module.exports = {success, apiMsg, welcomeMsg};
